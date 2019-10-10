4685947331bc2f5a39e18efc2c6d2f34911dc68f
adindasrah <adindasarah.asam@mail.unpas.ac.id>
2019-10-10 20:16:52 +0700

    video 8 selesai, cek lagi error

--- app/init.php
+++ app/init.php
@@ -2,5 +2,6 @@
 
 require_once 'core/App.php'; //huruf besar menandakan kelas
 require_once 'core/Controller.php';
+require_once 'core/Database.php';
 
-require_once 'core/Constants.php';
+require_once 'config/config.php';
