9b13057f75580ae724edc0b343e3738e405acee3
adindasrah <adindasarah.asam@mail.unpas.ac.id>
2019-10-11 08:53:18 +0700

    Video 9 selesai, menambahkan button tambah data ke database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -14,9 +14,24 @@ class Mahasiswa_model{
 	}
 
 	public function getMahasiswaById($id){
-		$this->db->query('SELECT * FROM' . $this->table . ' WHERE id=:id');
+		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
 		$this->db->bind('id', $id);
 		return $this->db->single();
+	}
+
+	public function tambahDataMahasiswa($data){
+		$query = "INSERT INTO mahasiswa VALUES
+		('', :nama, :nrp, :email, :jurusan)";
+
+		$this->db->query($query);
+		$this->db->bind('nama', $data['nama']);
+		$this->db->bind('nrp', $data['nrp']);
+		$this->db->bind('email', $data['email']);
+		$this->db->bind('jurusan', $data['jurusan']);
+
+		$this->db->execute();
+
+		return $this->db->rowCount();
 	}
 
 
