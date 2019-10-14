5a8f8449869f2e8ea5ebd68857df26ac1c70249a
adindasrah <adindasarah.asam@mail.unpas.ac.id>
2019-10-14 19:57:26 +0700

    Video 10 selesai, menambahkan flasher untuk tambah data mahasiswa

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if( !session_id() ) {
+	session_start();
+} 
+
 require_once '../app/init.php'; // init untuk teknik bootstrapping (memanggil seluruh file yang dbutuhkan)
 
 $app = new App;
